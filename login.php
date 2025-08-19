0795c52210bba47c5f340c0c3d3b9b471665bb25
10GOMENDOZA <tmeza5839@gmail.com>
2025-08-19 16:37:05 -0500

    cambios en el login

--- login.php
+++ login.php
@@ -13,17 +13,16 @@
 <body>
     <div class="container">
         <div class="izquierda">
-            <h1>Bienvenido</h1>
             <img src="img/mesa_ayuda.jpg" alt="imagen mesa de ayuda">
         </div>
         <div class="derecha">
             <h2>Iniciar Sesión</h2>
             <form action="procesar_login.php">
                 <div class="username">
-                    <input type="text" name="usuario" placeholder="ingrese usuario">
+                    <input type="text" name="usuario" placeholder="ingrese su  usuario">
                     <label>Usuario</label>
                     <div class="username">
-                        <input type="password" name="usuario" placeholder="ingrese contraseña">
+                        <input type="password" name="usuario" placeholder="ingrese su contraseña">
                         <label>Contrasena</label>
 
                         <input type="submit" value="Enviar">
