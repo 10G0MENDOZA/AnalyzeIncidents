007c3d5269a9bcc4c5c6e855f5c5deaf549d15f8
10GOMENDOZA <tmeza5839@gmail.com>
2025-08-22 11:01:13 -0500

    agg vista de exito apra la creacion de usuario falta todvia correciones el el css

---
+++ creacion_exitosa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creación Exitosa</title>
+    <link rel="icon" href="img/Logo_Avance.png">
+    <link rel="stylesheet" href="css/creacion_exitosa.css">
+</head>
+<body>
+    <div class="exito">
+        <h1>¡Usuario creado exitosamente! </h1>
+        <p>Se ha registrado correctamente tu cuenta en <strong>AnalyzeIncidents</strong>.</p>
+        <p>Recibirás un correo con tus datos de acceso y un enlace directo para iniciar sesión.</p>
+    </div>
+</body>
+</html>
